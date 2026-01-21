5c2d852a1935ac54c6c2fa5f261bdf7b82b07054
Jemima <jlopezj11@miumg.edu.gt>
2026-01-20 21:56:22 -0600

    commit create submit

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 
-const name = document.getElementById('name');
+const named = document.getElementById('named');
 const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
 const consent = document.getElementById('consent');
 const submit = document.getElementById('submit');
+
+submit.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (named.value && lastName.value && email.value && consent.checked && message.value) {           
+        alert("Message Sent!\nThanks for completing the form. We'll be in touch soon!");
+    } else {
+        alert("Please fill out all required fields and consent to being contacted.");
+    }  
+});         
