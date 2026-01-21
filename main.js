f8dc17ba5536b3254da0970a78c362436faf0ae2
Jemima <jlopezj11@miumg.edu.gt>
2026-01-20 21:38:04 -0600

    commit create const

---
+++ main.js
@@ -0,0 +1,6 @@
+
+const name = document.getElementById('name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const consent = document.getElementById('consent');
+const submit = document.getElementById('submit');
